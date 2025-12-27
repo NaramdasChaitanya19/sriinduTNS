780c7ef7e70cf1ee3638daaa5dba6d8d58d98249
CHAITANYA N <CHAITANYA N@LAPTOP-9J2EMKK9>
2025-12-27 13:28:47 +0530

    introloop

---
+++ Tnshyd/src/com/tnsif/intro/introloop/Dome.java
@@ -0,0 +1,18 @@
+package com.tnsif.intro.introloop;
+
+public class Dome {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+			}
+		while(i<=10);
+	}
+
+
+
+	}
+
+
---
+++ Tnshyd/src/com/tnsif/intro/introloop/While.java
@@ -0,0 +1,18 @@
+package com.tnsif.intro.introloop;
+
+public class While {
+
+	public static void main(String[] args) {
+
+			int a=2;
+			while(a<11) {
+				System.out.println(a);
+				a=a+2;
+		}
+
+	}
+	
+
+	}
+
+
