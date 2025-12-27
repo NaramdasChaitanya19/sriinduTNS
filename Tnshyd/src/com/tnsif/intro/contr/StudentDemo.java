8d608cffb73cadb054b93c34ad329d03979dfc9a
CHAITANYA N <CHAITANYA N@LAPTOP-9J2EMKK9>
2025-12-27 12:14:23 +0530

    constructor

---
+++ Tnshyd/src/com/tnsif/intro/contr/StudentDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.intro.contr;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		student st=new student();
+		student st1=new student(100);
+		student st2=new student(90,80);
+
+	}
+
+}
---
+++ Tnshyd/src/com/tnsif/intro/contr/student.java
@@ -0,0 +1,16 @@
+package com.tnsif.intro.contr;
+
+public class student {
+	int sid=101;
+	String sname="Chaithu";
+	student(){
+		System.out.println("The student nam:"+sname);
+		}
+	student(int a){
+		System.out.println("Marks "+a+" got to "+sname);
+	}
+student(int a,int b){
+		System.out.println("value1 "+a+" value2 "+b);
+		
+}
+}
