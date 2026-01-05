d32f04e45e3467197c4b9b58f31d7fbd98a40bc3
CHAITANYA N <CHAITANYA N@LAPTOP-9J2EMKK9>
2026-01-05 20:52:36 +0530

    This keyword

---
+++ Tnshyd/src/com/tnsif/intro/keyword/thisDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.intro.keyword;
+
+public class thisDemo {
+
+	public static void main(String[] args) {
+
+		thiss obje=new thiss();
+		obje.show();
+
+	}
+
+	}
+
+
---
+++ Tnshyd/src/com/tnsif/intro/keyword/thiss.java
@@ -0,0 +1,10 @@
+package com.tnsif.intro.keyword;
+
+public class thiss {
+	int a=10;
+	public void show() {
+		int a=39;
+		System.out.println(a);
+		System.out.println(this.a);
+	}
+}
