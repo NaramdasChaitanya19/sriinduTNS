84fcf04990ad91c7a6c84b57253e3546977de82b
CHAITANYA N <CHAITANYA N@LAPTOP-9J2EMKK9>
2026-02-05 20:49:33 +0530

    wrapper

---
+++ Tnshyd/src/com/tnsif/intro/wrapper/AutounBoxing.java
@@ -0,0 +1,15 @@
+package com.tnsif.intro.wrapper;
+
+public class AutounBoxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Double b=23.4567;
+		double g=b;
+		System.out.println(g);
+	}
+
+
+	}
+
+
