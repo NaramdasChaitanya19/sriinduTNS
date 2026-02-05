b53934b3156ee779c57f29bbef6ff5bfe627f4b0
CHAITANYA N <CHAITANYA N@LAPTOP-9J2EMKK9>
2026-02-05 22:49:41 +0530

    task

---
+++ Tnshyd/src/com/tnsif/assessment/typeConversion.java
@@ -0,0 +1,18 @@
+package com.tnsif.assessment;
+
+public class typeConversion {
+
+	public static void main(String[] args) {
+
+		//Implicit type conversion
+				int a=10;
+				double b=a;
+				System.out.println(b);
+			//Explicit type conversion
+			float c=10.78f;
+			int s=(int)c;
+			System.out.println(s);
+
+		}
+
+}
