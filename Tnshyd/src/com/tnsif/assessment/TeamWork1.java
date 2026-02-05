8f18e26466340f4d069b148f035ced090c5d74f6
CHAITANYA N <CHAITANYA N@LAPTOP-9J2EMKK9>
2026-02-05 23:23:17 +0530

    Team work

--- Tnshyd/src/com/tnsif/assessment/TeamWork1.java
+++ Tnshyd/src/com/tnsif/assessment/TeamWork1.java
@@ -3,7 +3,28 @@ package com.tnsif.assessment;
 public class TeamWork1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		char[][] word= {{'T','E','A','M'},{'W','O','R','K'}};
+		String text="";
+		for (int i = 0; i < word.length; i++) {
+		    for (int j = 0; j < word[i].length; j++) {
+		        text+=word[i][j];
+		        }
+		}
+		    System.out.println(text);
+		
+		String reverse="";
+
+	    for (int i = text.length() - 1; i >= 0; i--) {
+	        reverse +=text.charAt(i);
+	    }
+
+	    System.out.println(reverse);
+
+
+		}
+	
+
 
 	}
 
