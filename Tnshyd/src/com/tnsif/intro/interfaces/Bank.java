3bbf30d3956fc8555bcde389822be736354a4e6a
CHAITANYA N <CHAITANYA N@LAPTOP-9J2EMKK9>
2026-02-06 16:39:46 +0530

    bankaccount(interface)

---
+++ Tnshyd/src/com/tnsif/intro/interfaces/Bank.java
@@ -0,0 +1,10 @@
+package com.tnsif.intro.interfaces;
+
+public interface Bank {
+	int MIN_BALANCE=1000;
+	int DEPOSITE_LIMIT=25000;
+	void deposite(Account account,double amount);
+	void withdraw(Account account,double amount);
+
+
+}
---
+++ Tnshyd/src/com/tnsif/intro/interfaces/BankApplication.java
@@ -0,0 +1,14 @@
+package com.tnsif.intro.interfaces;
+
+public class BankApplication {
+	public static void main(String[] args) {
+		
+		Bank bank=new BankImplement();
+		Account account=new Account(1234,"Chaitanya",5000,bank);
+		bank.deposite(account,5000);
+		bank.withdraw(account, 3000);
+		System.out.println(account);
+}
+
+}
+
